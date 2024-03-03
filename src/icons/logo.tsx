d4a33597e256a1817b664540ee842025b9379b94
Jaycom17 <juanorejuela499@gmail.com>
2024-03-03 18:57:25 -0500

    first commit

---
+++ src/icons/logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div >
+      <img className="rounded-full" height={"80px"} width={"80px"} src="./logoSS.png" alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
