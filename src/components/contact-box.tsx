d24164cb0fcffa2dca06b01b5c5aed2c6f416b17
Jaycom17 <juanorejuela499@gmail.com>
2024-03-10 14:58:31 -0500

    almost done

--- src/components/contact-box.tsx
+++ src/components/contact-box.tsx
@@ -7,11 +7,11 @@ const ContactInfo = () => {
     <div className=" col-span-1 bg-primary-300 rounded-[30px] px-8 py-[76px] flex flex-col gap-[30px] w-full content-center justify-center">
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Mail /> <a href="mailto:email">email</a>
+        <Mail /> <a href="mailto:email">joanrosero@unicauca.edu.co</a>
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Phone /> <a href="">+1 (378) 400-1234</a>
+        <Phone /> <a href="">+57 3135739668</a>
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,4 @@
-import Facebook from "../icons/facebook.tsx";
-import Instagram from "../icons/instagram.tsx";
-import LinkedIn from "../icons/linkedin.tsx";
 import Logo from "../icons/logo.tsx";
-import Twitter from "../icons/twitter.tsx";
 
 const Footer = () => {
   return (
@@ -17,17 +13,22 @@ const Footer = () => {
             Permitenos mostrarte el poder del espacio.
           </p>
           <div className="flex gap-5 md:gap-[54px]">
-            <a className="ease-in-out hover:scale-110 duration-300" href="/">
-              <Facebook />
-            </a>
-            <a className="ease-in-out hover:scale-110 duration-300" href="/">
-              <Twitter />
-            </a>
-            <a className="ease-in-out hover:scale-110 duration-300" href="/">
-              <LinkedIn />
-            </a>
-            <a className="ease-in-out hover:scale-110 duration-300" href="/">
-              <Instagram />
+            <a className="ease-in-out hover:scale-110 duration-300" href="https://github.com/Jaycom17">
+            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="#718096"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path
+                              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                            ></path>
+                          </svg>
             </a>
           </div>
         </div>
@@ -78,10 +79,10 @@ const Footer = () => {
           </h5>
           <div className="flex flex-col gap-6">
             <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
-              contact@interno.com
+              joanrosero@unicauca.edu.co
             </p>
             <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
-              (123) 456 - 7890
+              +57 3135739668
             </p>
           </div>
         </div>
