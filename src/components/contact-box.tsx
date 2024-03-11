92cdf5311c7a7e5c10b1c55a626385e499599610
Jaycom17 <juanorejuela499@gmail.com>
2024-03-11 15:19:31 -0500

    done

--- src/components/contact-box.tsx
+++ src/components/contact-box.tsx
@@ -7,15 +7,15 @@ const ContactInfo = () => {
     <div className=" col-span-1 bg-primary-300 rounded-[30px] px-8 py-[76px] flex flex-col gap-[30px] w-full content-center justify-center">
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Mail /> <a href="mailto:email">joanrosero@unicauca.edu.co</a>
+        <Mail /> <a href="mailto:joanrosero@unicauca.edu.co">joanrosero@unicauca.edu.co</a>
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Phone /> <a href="">+57 3135739668</a>
+        <Phone /> <p>+57 3135739668</p>
       </h5>
       <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Web /> <a href="/">link</a>
+        <Web /> <a href="https://softwarespace-lovat.vercel.app/">softwarespace-lovat.vercel.app/</a>
       </h5>
     </div>
   );
